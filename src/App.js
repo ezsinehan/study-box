e198992484e4535bbb009d2939bd0c9ebf6c9320
Sinehan Ezhilmuthu <ezsinehan@gmail.com>
2024-01-20 14:40:32 -0800

    basic cube

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Cube from "./components/Cube";
+
+function App() {
+  return (
+    <div className="App">
+      <Cube />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cube.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "../styles/Cube.css";
+
+const Cube = () => {
+  const [activeFace, setActiveFace] = useState("");
+
+  const isFaceActive = (face) => {
+    return activeFace === `show-${face}`;
+  };
+
+  return (
+    <div>
+      <div className="scene">
+        <div className={`cube ${activeFace}`}>
+          <div className="cube__face cube__face--front">
+            {isFaceActive("front") && "front"}
+          </div>
+          <div className="cube__face cube__face--back">
+            {isFaceActive("back") && "back"}
+          </div>
+          <div className="cube__face cube__face--right">
+            {isFaceActive("right") && "right"}
+          </div>
+          <div className="cube__face cube__face--left">
+            {isFaceActive("left") && "left"}
+          </div>
+          <div className="cube__face cube__face--top">
+            {isFaceActive("top") && "top"}
+          </div>
+          <div className="cube__face cube__face--bottom">
+            {isFaceActive("bottom") && "bottom"}
+          </div>
+        </div>
+      </div>
+
+      <div className="cube-controls">
+        <button onClick={() => setActiveFace("show-front")}>Front</button>
+        <button onClick={() => setActiveFace("show-back")}>Back</button>
+        <button onClick={() => setActiveFace("show-right")}>Right</button>
+        <button onClick={() => setActiveFace("show-left")}>Left</button>
+        <button onClick={() => setActiveFace("show-top")}>Top</button>
+        <button onClick={() => setActiveFace("show-bottom")}>Bottom</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cube;
