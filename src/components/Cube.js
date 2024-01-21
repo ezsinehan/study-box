6c00fbf3017a4e183264814510bb6678c88d84dd
Sinehan Ezhilmuthu <ezsinehan@gmail.com>
2024-01-21 08:40:05 -0800

    cube descriptions

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -14,6 +14,21 @@ const Cube = () => {
     return activeFace === `show-${faceName}`;
   };
 
+  const descriptions = {
+    Studybox:
+      "Studybox is an website system that employs cognitive structuring to categorize and retrieve academic resources efficiently, optimizing your knowledge acquisition process.",
+    Feynman:
+      "The Feynman Technique leverages didactic pedagogy, simplifying and deconstructing sophisticated subjects into fundamental principles for enhanced cognitive assimilation.",
+    Flashcards:
+      "Flashcards utilize active recall and spaced repetition, two neurologically beneficial methods, for enhancing the retention of discrete pieces of information.",
+    Pomodoro:
+      "Pomodoro Technique is a time management methodology that employs fixed interval scheduling, optimizing cognitive performance through structured bursts of focused activity.",
+    SecondBrain:
+      "Second Brain is a digital information management system, functioning as an external cognitive storage device, facilitating efficient information retrieval and mental clarity.",
+    Binauralbeats:
+      "Binaural Beats are an auditory processing artifact, stemming from the brain's neural integration of two distinct sound frequencies, which purportedly induces altered states of consciousness.",
+  };
+
   return (
     <div>
       <div className="cube-container">
@@ -23,37 +38,69 @@ const Cube = () => {
               className="cube__face cube__face--front"
               onClick={() => navigate("/")}
             >
-              {isFaceActive("front") && "Studybox"}
+              {isFaceActive("front") && (
+                <div className="face-content">
+                  <div className="title">Studybox</div>
+                  <div className="description">{descriptions.Studybox}</div>
+                </div>
+              )}
             </div>
             <div
               className="cube__face cube__face--back"
               onClick={() => navigate("/Feynman")}
             >
-              {isFaceActive("back") && "Feynman"}
+              {isFaceActive("back") && (
+                <div className="face-content">
+                  <div className="title">Feynman</div>
+                  <div className="description">{descriptions.Feynman}</div>
+                </div>
+              )}
             </div>
             <div
               className="cube__face cube__face--right"
               onClick={() => navigate("/Flashcards")}
             >
-              {isFaceActive("right") && "Flashcards"}
+              {isFaceActive("right") && (
+                <div className="face-content">
+                  <div className="title">Flashcards</div>
+                  <div className="description">{descriptions.Flashcards}</div>
+                </div>
+              )}
             </div>
             <div
               className="cube__face cube__face--left"
               onClick={() => navigate("/Pomodoro")}
             >
-              {isFaceActive("left") && "Pomodoro"}
+              {isFaceActive("left") && (
+                <div className="face-content">
+                  <div className="title">Pomodoro</div>
+                  <div className="description">{descriptions.Pomodoro}</div>
+                </div>
+              )}
             </div>
             <div
               className="cube__face cube__face--top"
               onClick={() => navigate("/SecondBrain")}
             >
-              {isFaceActive("top") && "SecondBrain"}
+              {isFaceActive("top") && (
+                <div className="face-content">
+                  <div className="title">SecondBrain</div>
+                  <div className="description">{descriptions.SecondBrain}</div>
+                </div>
+              )}
             </div>
             <div
               className="cube__face cube__face--bottom"
               onClick={() => navigate("/binauralbeats")}
             >
-              {isFaceActive("bottom") && "Binuralbeats"}
+              {isFaceActive("bottom") && (
+                <div className="face-content">
+                  <div className="title">Binauralbeats</div>
+                  <div className="description">
+                    {descriptions.Binauralbeats}
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
